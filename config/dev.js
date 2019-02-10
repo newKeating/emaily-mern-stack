6a49c2414985e5abe3a48e323109433c7dfa6f9f
Peter Kim <kroits09@gmail.com>
2019-02-10 22:45:02 +0900

    no message

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID:
+    "22678646007-kbvh4ub5ame1lmcjj3inv3eb6istv3s1.apps.googleusercontent.com",
+  googleClientSecret: "6jIXWM2d1PhznsYfbXr04V9M",
+  mongoURI:
+    "mongodb://peterwns:1115gate@ds221435.mlab.com:21435/emaily-dev-peter",
+  cookieKey: "iaosdfaksfuiwelmcxioaewudoi9102830912k3l12jsadfsa"
+};
